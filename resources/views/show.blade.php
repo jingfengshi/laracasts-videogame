fa7b9d4c85307f4051e4510997197d3de69356d2
jasper <240023810@qq.com>
2020-07-15 17:12:00 +0800

    获取游戏api接口数据

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <div class="container mx-auto px-4">
-        <div class="game-details border-b border-gray-800 pb-12 flex">
+        <div class="game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
             <div class="flex-none">
                 <img src="/ff7.jpg" alt="cover">
             </div>
-            <div class="ml-12 mr-64">
-                <h2 class="font-semibold text-4xl">
+            <div class="lg:ml-12 lg:mr-64">
+                <h2 class="font-semibold text-4xl leading-tight mt-1">
                     Final Fantasy VII Remake
                 </h2>
                 <div class="text-gray-400">
@@ -33,7 +33,7 @@
                         </div>
                         <div class="ml-4 text-xs">Critic<br> Score</div>
                     </div>
-                    <div class="flex items-center space-x-4">
+                    <div class="flex items-center space-x-4 mt-8 lg:mt-0 lg:ml-12">
                         <div class="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center">
                             <a href="#" class="hover:text-gray-400">a</a>
                         </div>
@@ -52,7 +52,8 @@
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus eaque hic inventore molestias rem. Ab architecto at corporis doloremque dolorum, error, hic iusto laborum natus placeat sit soluta sunt unde.
                 </p>
                 <div class="mt-12">
-                    <button class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:text-blue-600 rounded transition ease-in-out duration-150">
+                    <button
+                        class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:text-blue-600 rounded transition ease-in-out duration-150">
                         <span class="ml-2">Play Trailer</span>
                     </button>
                 </div>
@@ -60,7 +61,7 @@
         </div><!-- end game details -->
         <div class="images-container border-b border-gray-800 pb-12 mt-8">
             <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Images</h2>
-            <div class="grid grid-cols-3 gap-12  mt-8">
+            <div class="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-12  mt-8">
                 <div>
                     <a href="#">
                         <img src="/ff7.jpg" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
